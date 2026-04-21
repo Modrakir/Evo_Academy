9af666010eedd356b08d82f21a5b835b823cd48a
Пономарев Михаил <maponom@gmail.com>
2026-04-21 16:01:15 +0300

    Task1.7

--- EvoApp6/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Book } from './components/book/book';
-import { App } from './app';
-
-export const routes: Routes = [
-    {path: '', component: App},
-    {path: 'book', component: Book}
-];
