03c2c05639526f27ad331c30e46816583e78c11c
Пономарев Михаил <maponom@gmail.com>
2026-04-21 16:15:50 +0300

    Task1.6

--- EvoApp/src/app/modules/page/components/item/item.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-item',
-  standalone: false,
-  templateUrl: './item.html',
-  styleUrl: './item.css',
-})
-export class Item {}
