4a3acfbff543ebb2fc0484706a5f76cdb1e900ae
Пономарев Михаил <maponom@gmail.com>
2026-04-21 15:58:47 +0300

    Task1.7

--- FirstEvoApp/src/app/add-five-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddFivePipe } from './add-five-pipe';
-
-describe('AddFivePipe', () => {
-  it('create an instance', () => {
-    const pipe = new AddFivePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
